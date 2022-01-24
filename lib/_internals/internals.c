d1269e99999be160f6f341ff3f7ec53d8016302b
0xdw <12568779+0xdw@users.noreply.github.com>
2022-01-24 23:55:17 +0530

    Replace STATUS_CHECK with NODE_API_CALL

--- lib/_internals/internals.c
+++ lib/_internals/internals.c
@@ -1,27 +1,22 @@
 #include "internals.h"
 
 bool is_array(napi_env env, napi_value value) {
-  bool _is_array = 0;
+  bool _is_array = false;
   napi_status status = napi_is_array(env, value, &_is_array);
-  STATUS_CHECK
+  if (status != napi_ok) return false;
   return _is_array;
 }
 
 napi_value array_slice(napi_env env, napi_value array, size_t length,
                        size_t from, size_t to) {
-  napi_status status;
-
   napi_value value;
-  status = napi_create_array(env, &value);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_create_array(env, &value));
 
   size_t ii = 0;
   napi_value n;
   for (size_t i = from; i < to && i < length; i++) {
-    status = napi_get_element(env, array, i, &n);
-    STATUS_CHECK
-    status = napi_set_element(env, value, ii, n);
-    STATUS_CHECK
+    NODE_API_CALL(env, napi_get_element(env, array, i, &n));
+    NODE_API_CALL(env, napi_set_element(env, value, ii, n));
     ii++;
   }
 
