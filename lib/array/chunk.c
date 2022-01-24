c44e9ed31c8ee7e1384208d88c3a7b01e7b6dea6
0xdw <12568779+0xdw@users.noreply.github.com>
2022-01-24 23:51:50 +0530

    Replace STATUS_CHECK with NODE_API_CALL

--- lib/array/chunk.c
+++ lib/array/chunk.c
@@ -3,12 +3,9 @@
 #include "../_internals/internals.h"
 
 napi_value lodash_chunk(napi_env env, napi_callback_info info) {
-  napi_status status;
-
   size_t argc = 2;
   napi_value args[2];
-  status = napi_get_cb_info(env, info, &argc, args, NULL, NULL);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
 
   if (argc < 2) {
     napi_throw_type_error(env, NULL, WRONG_ARGUMENTS_ERROR);
@@ -21,8 +18,7 @@ napi_value lodash_chunk(napi_env env, napi_callback_info info) {
   }
 
   napi_valuetype valuetype1;
-  status = napi_typeof(env, args[1], &valuetype1);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_typeof(env, args[1], &valuetype1));
 
   if (valuetype1 != napi_number) {
     napi_throw_type_error(env, NULL, "Second argument should be a number");
@@ -32,26 +28,22 @@ napi_value lodash_chunk(napi_env env, napi_callback_info info) {
   napi_value array = args[0];
 
   uint32_t length;
-  status = napi_get_array_length(env, array, &length);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_get_array_length(env, array, &length));
 
   int32_t size_value;
-  status = napi_get_value_int32(env, args[1], &size_value);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_get_value_int32(env, args[1], &size_value));
   size_t size = MAX(size_value, 0);
 
   size_t index = 0;
   size_t rindex = 0;
 
   napi_value result;
-  status = napi_create_array(env, &result);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_create_array(env, &result));
 
   while (index < length) {
     size_t i = index + size;
     napi_value s = array_slice(env, array, length, index, i);
-    status = napi_set_element(env, result, rindex++, s);
-    STATUS_CHECK
+    NODE_API_CALL(env, napi_set_element(env, result, rindex++, s));
     index = i;
   }
 
--- lib/array/compact.c
+++ lib/array/compact.c
@@ -3,12 +3,9 @@
 #include "../_internals/internals.h"
 
 napi_value lodash_compact(napi_env env, napi_callback_info info) {
-  napi_status status;
-
   size_t argc = 1;
   napi_value args[1];
-  status = napi_get_cb_info(env, info, &argc, args, NULL, NULL);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
 
   if (argc < 1) {
     napi_throw_type_error(env, NULL, WRONG_ARGUMENTS_ERROR);
@@ -21,32 +18,28 @@ napi_value lodash_compact(napi_env env, napi_callback_info info) {
   }
 
   uint32_t length;
-  status = napi_get_array_length(env, args[0], &length);
-  STATUS_CHECK
+  NODE_API_CALL(env, napi_get_array_length(env, args[0], &length));
 
   napi_value array = args[0];
 
   napi_value output_array;
-  status = napi_create_array_with_length(env, NAPI_AUTO_LENGTH, &output_array);
-  STATUS_CHECK
+  NODE_API_CALL(
+      env, napi_create_array_with_length(env, NAPI_AUTO_LENGTH, &output_array));
 
   uint32_t result_index = 0;
 
   for (size_t i = 0; i < length; i++) {
     napi_value value;
-    status = napi_get_element(env, array, i, &value);
-    STATUS_CHECK
+    NODE_API_CALL(env, napi_get_element(env, array, i, &value));
 
     napi_valuetype type;
-    status = napi_typeof(env, value, &type);
-    STATUS_CHECK
+    NODE_API_CALL(env, napi_typeof(env, value, &type));
     // https://developer.mozilla.org/en-US/docs/Glossary/Falsy
     // false, 0, -0, 0n, "", '', ``, null, undefined, NaN
 
     if (type == napi_boolean) {
       bool result;
-      status = napi_get_value_bool(env, value, &result);
-      STATUS_CHECK
+      NODE_API_CALL(env, napi_get_value_bool(env, value, &result));
       if (result == false) continue;
     }
 
@@ -54,16 +47,15 @@ napi_value lodash_compact(napi_env env, napi_callback_info info) {
     // zero.
     if (type == napi_number) {
       int32_t number;
-      status = napi_get_value_int32(env, value, &number);
-      STATUS_CHECK
+      NODE_API_CALL(env, napi_get_value_int32(env, value, &number));
       if (number == 0 || number == -0) continue;
     }
 
     if (type == napi_bigint) {
       int64_t number;
       bool loss_less;
-      status = napi_get_value_bigint_int64(env, value, &number, &loss_less);
-      STATUS_CHECK
+      NODE_API_CALL(
+          env, napi_get_value_bigint_int64(env, value, &number, &loss_less));
       if (number == 0) continue;
     }
 
@@ -71,15 +63,14 @@ napi_value lodash_compact(napi_env env, napi_callback_info info) {
     bool str_found = false;
     if (type == napi_string) {
       size_t str_buffer_size;
-      status =
-          napi_get_value_string_utf8(env, value, NULL, 0, &str_buffer_size);
-      STATUS_CHECK
+      NODE_API_CALL(env, napi_get_value_string_utf8(env, value, NULL, 0,
+                                                    &str_buffer_size));
 
       str_buffer = (char*)malloc(str_buffer_size + 1);
       size_t str_result;
-      status = napi_get_value_string_utf8(env, value, str_buffer,
-                                          str_buffer_size + 1, &str_result);
-      STATUS_CHECK
+      NODE_API_CALL(
+          env, napi_get_value_string_utf8(env, value, str_buffer,
+                                          str_buffer_size + 1, &str_result));
 
       str_found = true;
       if (*str_buffer == '\0') {
@@ -91,8 +82,8 @@ napi_value lodash_compact(napi_env env, napi_callback_info info) {
 
     if (type == napi_null || type == napi_undefined) continue;
 
-    status = napi_set_element(env, output_array, result_index, value);
-    STATUS_CHECK
+    NODE_API_CALL(env,
+                  napi_set_element(env, output_array, result_index, value));
     result_index++;
     if (str_found) {
       str_found = false;
